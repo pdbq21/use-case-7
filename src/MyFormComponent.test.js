1485e4dad2da28120d592d93ff0b7b7eb867809b
Ruslan Yakymovych <r_yakymovych-itssv@kw.com>
2023-07-24 11:56:23 +0300

    UC#7 Create documentation

--- src/MyFormComponent.test.js
+++ src/MyFormComponent.test.js
@@ -1,19 +1,6 @@
 import { render, screen, fireEvent } from '@testing-library/react';
 import MyFormComponent from './MyFormComponent';
 
-/*
-1.	Submit the form with all fields filled in correctly: a name of 3 or more characters, a valid email, 'Agree to Terms' checked, and a gender selected.
-2.	Submit the form with a very long valid name to check if the form can handle names of any length.
-3.	Submit the form with a complex email address that is valid (e.g., test.name+alias@example.co.uk) to test the robustness of the email validation.
-4.	Change the gender from male to female and submit the form with all other fields filled in correctly.
-5.	Re-submit the form after an initial successful submission with all fields filled in correctly.
-6.	Submit the form with the 'Name' field left blank.
-7.	Submit the form with an invalid email address (e.g., without the "@" symbol).
-8.	Submit the form without checking the 'Agree to Terms' checkbox.
-9.	Submit the form without selecting a gender.
-10.	Submit the form with a name that is less than 3 characters long.
-*/
-
 describe('<MyFormComponent/>', () => {
   const mockData = {
     name: 'TestName',
@@ -140,6 +127,7 @@ describe('<MyFormComponent/>', () => {
     fireEvent.change(name, {target: {value: 'ad' }})
     fireEvent.change(email, {target: {value: mockData.email }})
     fireEvent.click(agreeTerms)
+    fireEvent.click(genders[0])
     fireEvent.click(submit)
 
     const genderError = screen.getByText(/Name must be at least 3 characters./i)
